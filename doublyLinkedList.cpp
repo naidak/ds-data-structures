b55a395f3c7e2bbd7732f0c0e35874e21cc144ce
naidak <57109234+naidak@users.noreply.github.com>
2022-05-20 17:14:11 +0200

    Function InsertAtTail added

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -24,6 +24,27 @@ void InsertAtHead(int x)
 	head = temp;
 }
 
+void InsertAtTail(int x)
+{
+	Node* temp = new Node();
+	Node* temp1 = head;
+	temp->data = x;
+	temp->next = nullptr;
+	temp->prev = nullptr;
+	if (head == nullptr)
+	{
+		head = temp;
+		return;
+	}
+
+	while (temp1->next!=nullptr)
+		temp1 = temp1->next;
+	
+	temp1->next = temp;
+	temp->prev = temp1;
+	
+}
+
 void print(Node * node)
 {
 	while (node!=nullptr)
@@ -36,7 +57,9 @@ void print(Node * node)
 void reversePrint(Node* node)
 {
 	while (node->next != nullptr)
+	{
 		node = node->next;
+	}
 
 	while (node != nullptr)
 	{
@@ -46,13 +69,16 @@ void reversePrint(Node* node)
 }
 
 int main() {
-	InsertAtHead(1);
+	InsertAtTail(1);
 	InsertAtHead(4);
-	InsertAtHead(7);
+	InsertAtTail(7);
+	InsertAtHead(8);
+	InsertAtHead(9);
+	InsertAtTail(3);
 
-	print(head);
-	cout << endl;
-	reversePrint(head);
+	print(head); //9 8 4 1 7 3
+	cout << endl; 
+	reversePrint(head); //3 7 1 4 8 9
 
 	system("pause>0");
 	return 0;
